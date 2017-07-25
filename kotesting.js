040620108dede55b17fc019a7248f8f524c54472
jorgewithls <30378077+jorgewithls@users.noreply.github.com>
2017-07-24 21:28:49 -0700

    Established all baseline JS
    Major commit - play one game equivalent. No animation or custom bindings.

--- kotesting.js
+++ kotesting.js
@@ -1,27 +1,29 @@
 function AppViewModel() {
   var self = this;
-  var birthday = "";
-  var sign = "";
+  var birthday = ""; // for storing user input birthday
+  var sign = ""; // for zodiac message
+  var message = ""; // for verdict message at the end
   var points = 0;
-  var result = "";
-  self.submit = ko.observable(false);
+  self.total = ko.observable(0); // for computing verdict message
+  self.submit = ko.observable(false); // for determining if visibility
+  self.email = ko.observable(false); // for determining visibility of email form 
 
   self.age = ko.observable();
   self.games = ko.observable(false);
-  self.gamesF = function() {return true;}
+  self.gamesF = function() {return true;}; // toggles button click data being sent back to observable
   self.cook = ko.observable(false);
   self.cookF = function() {return true;}
   self.bday = ko.observable();
+
+  // determines if bday has been filled out or not and controls visibility accordingly
   self.visibility = function(value) {
     var filled = ko.utils.unwrapObservable(value);
     var test = filled != null || filled != undefined ? true : false;
     return test;
   };
-  self.trigger = function() {
-    self.score(self.result2);
-  }
+
+  // compiles scores from different answer and comes to a point total
   self.score = function() {
-    console.log("beginning of score");
     if (ko.utils.unwrapObservable(self.age) == '16-20') {  // based off of age
       points += 4;
     } else if (ko.utils.unwrapObservable(self.age) == '21-25') {
@@ -31,8 +33,9 @@ function AppViewModel() {
     } else if (ko.utils.unwrapObservable(self.age) == '26-30') {
       points++;
     }
-    if (ko.utils.unwrapObservable(self.games)) {points++;};
-    if (ko.utils.unwrapObservable(self.cook)) {points++;};
+    if (ko.utils.unwrapObservable(self.games)) {points++;}; // based off of games
+    if (ko.utils.unwrapObservable(self.cook)) {points++;}; // based off of cooking
+    // based off of sign
     if (sign == "an Aquarius") points += 4;
     if (sign == "a Pisces") points += 2;
     if (sign == "an Aries") points += 2;
@@ -45,13 +48,11 @@ function AppViewModel() {
     if (sign == "a Scorpio") points += 2;
     if (sign == "a Sagitarius") points += 2;
     if (sign == "a Capricorn") points += 2;
+    self.total(points);
     self.submit(true);
   };
-  self.result = function() {
-    if (points == 0) {
-      self.trigger();
-    }
-  };
+
+  // sorts out zodiac based on birthday
   self.zodiacFind = ko.computed(function() {
     if (ko.utils.unwrapObservable(self.bday) != null) {
       var str = ko.utils.unwrapObservable(self.bday);
@@ -69,6 +70,18 @@ function AppViewModel() {
       if ((birthday[0] == 11 && birthday[1] > 21) || (birthday[0] == 12 && birthday[1] <= 21)) sign = "a Sagitarius";
       if ((birthday[0] == 12 && birthday[1] > 21) || (birthday[0] == 1 && birthday[1] <= 19)) sign = "a Capricorn";
       return sign;
+    }
+  }, self);
+
+  // determines ending message based on points
+  self.verdict = ko.computed(function() {
+    if (ko.utils.unwrapObservable(self.total) != 0) {
+      if (points < 3) message = "Sorry, doesn't look like you're my type.";
+      if (points >= 4 && points < 6) message = "Sorry, you're not exactly who I'm looking for right now.";
+      if (points >= 6 && points < 8) message = "Hmm, check me out on Instagram @jorgealv.png";
+      if (points >= 8) message = "Looks like we may get along. Let's grab coffee sometime; fill out the form below:";
+      self.email(true);
+      return message;
     }
   }, self);
 }
